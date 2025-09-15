0c3e005f4c70c69aba5853bd7358dcaa0c45ea7f
Jay Jun <jaywujun1@gmail.com>
2025-09-15 18:10:28 -0400

    Finished Part 1

--- part1/knn.cpp
+++ part1/knn.cpp
@@ -18,6 +18,20 @@ constexpr float getCoordinate(Embedding_T e, size_t axis)
     return e;  // scalar case
 }
 
+Node* buildKD_assume_sorted(std::vector<std::pair<Embedding_T,int>>& items, int start, int end) {
+    if (start == end) return nullptr;
+
+    int mid = (start + end) / 2;
+    Node* root = new Node();
+    root->embedding = items[mid].first;
+    root->idx = items[mid].second;
+    root->left = buildKD_assume_sorted(items, start, mid);
+    root->right = buildKD_assume_sorted(items, mid + 1, end);
+
+    return root;
+
+}
+
 // Build a balanced KD‐tree by splitting on median at each level.
 Node* buildKD(std::vector<std::pair<Embedding_T,int>>& items, int depth) {
     /*
@@ -25,7 +39,10 @@ Node* buildKD(std::vector<std::pair<Embedding_T,int>>& items, int depth) {
     You should recursively construct the tree and return the root node.
     For now, this is a stub that returns nullptr.
     */
-    return nullptr;
+    std::sort(items.begin(), items.end(), [](const std::pair<Embedding_T,int>& a, const std::pair<Embedding_T,int>& b) {
+        return getCoordinate(a.first, 0) < getCoordinate(b.first, 0);
+    });
+    return buildKD_assume_sorted(items, 0, items.size());
 }
 
 
@@ -47,5 +64,32 @@ void knnSearch(Node *node,
     You should recursively traverse the tree and maintain a max-heap of the K closest points found so far.
     For now, this is a stub that does nothing.
     */
+   if (node == nullptr) return;
+
+    float qdist = distance(node->embedding, Node::queryEmbedding);
+    if ((int) heap.size() < K) {
+        heap.push(PQItem(qdist, node->idx));
+    }
+    else {
+        float hdist = heap.top().first;
+        if (hdist > qdist) {
+            heap.pop();
+            heap.push(PQItem(qdist, node->idx));
+        }
+    }
+    Node *close, *far;
+    if (node->embedding > Node::queryEmbedding) {
+        close = node->left;
+        far = node->right;
+    }
+    else {
+        close = node->right;
+        far = node->left;
+    }
+    knnSearch(close, depth + 1, K, heap);
+    float hdist = heap.top().first;
+    if (qdist < hdist || (int) heap.size() < K) {
+        knnSearch(far, depth + 1, K, heap);
+    }
     return;
 }
